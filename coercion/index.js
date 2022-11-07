13d49efb49ed6f4eec2598d4ae88b0f2b37299c4
adarsh1997shah <adarshshah01@gmail.com>
2022-11-07 23:46:33 +0530

    fix: intro coersion notes

--- coercion/index.js
+++ coercion/index.js
@@ -3,14 +3,21 @@
  * Either toString or valueOf func is invoked based on the operation that is being performed.
  * If the value is not a primitive it gets turns to primitive like object, array, func
  * The alogrithm of JS for coercion is recursive, i.e. if the output of first conversion is not a primitive then it converts again and it goes on until it gets to a primitive value
- * If the alorithm hint
-    - `is Number`
+ * If the algorithm hints
+    - `a Number`
         - first perform valueOf(), if it gives primitive,
         - then perform toString()
 
-    - `is String`
+    - `a String`
         - first perform toString(), if it gives primitive,
         - then perform valueOf()
 
-      If by performing both the operation we doesnt get primitive then it generally gives an error
+      If by performing both the operation we dont get primitive then it generally gives an error
+
+  * We have talked about a valueOf and toString a lot, so what those really are
+      - valueOf - it returns the primitive value, if it is called upon a non-primitive value then it usually returns the reference of that non-primitive type
+      
+      - toString() - it turns value to string
+
+  * Every primitive and non-primitive value have these two implemented by JS, until and unless they are overriden
  */
