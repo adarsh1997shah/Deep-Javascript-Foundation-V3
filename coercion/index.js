b7e5ce6be6f0dae6d301a015c4172f7871910079
adarsh1997shah <adarshshah01@gmail.com>
2022-11-07 21:44:35 +0530

    feat: toString notes add

---
+++ coercion/index.js
@@ -0,0 +1,4 @@
+/**
+ * Coercion is basically performed in two steps on either side for conditional statement.
+ * First toString func is performed and if it doesnt matches it goes for valueOf func.
+ */
---
+++ coercion/toString/index.js
@@ -0,0 +1,30 @@
+/**
+ * toString func returns a string converting the value it is called upon.
+ * Examples
+ */
+
+// Arrays
+console.log([].toString()); // ""
+console.log([1, 2, 3].toString()); // "1,2,3"
+console.log([[[], []], []].toString()); // ",,"
+console.log([null, undefined].toString()); // ","
+
+// Object
+const obj1 = {};
+const obj2 = { x: 2 };
+
+console.log(obj1.toString()); // "[object Object]"
+console.log(obj2.toString()); // "[object Object]"
+
+// Note
+// We can override a toString function to completely control what are object stringification look like.
+// If we want to override toString we can do so by creating a property on that particular object
+
+// obj1.toString = function () {
+// 	return 'Custom toString return';
+// };
+
+// So we can override the toString to return the actual contents of the object like so
+obj1.toString = function () {
+	return JSON.stringify(this);
+};
