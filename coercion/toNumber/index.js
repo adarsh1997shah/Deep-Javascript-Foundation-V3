0d609f86696d29ee4d4fba3a4b1bdf3df1f6123e
adarsh1997shah <adarshshah01@gmail.com>
2022-11-08 00:01:47 +0530

    feat: toNumber notes added

---
+++ coercion/toNumber/index.js
@@ -0,0 +1,35 @@
+/**
+ * Convert value to number
+ * Examples
+ */
+
+// We can use Number func to convert values to number
+// Operation to primitives
+console.log(Number('')); // 0
+console.log(Number('0')); // 0
+console.log(Number('-0')); // -0
+console.log(Number('   009 ')); // -9
+console.log(Number('0.')); // 0
+console.log(Number('.0')); // 0
+console.log(Number('.')); // NaN
+console.log(Number('0xaf')); // 175, handles other bases
+
+console.log(Number(false)); // 0
+console.log(Number(true)); // 1
+console.log(Number(null)); // 0
+console.log(Number(undefined)); // NaN
+
+// Operation to non-primitives
+// First it invokes with the `number` hint so the order is valueOf ---> toString
+// For [] and {} it return this
+// valueOf() { return this; }
+console.log(Number([''])); // so here first the valueOf will return the array reference and then toString performs and converts to "" and then when we convert that to number it gives 0
+
+console.log(Number(['0'])); // toString -> '0', toNumber -> 0
+console.log(Number(['-0'])); // toString -> '-0', toNumber -> -0
+console.log(Number([null])); // toString -> '', toNumber -> 0
+console.log(Number([undefined])); // toString -> '', toNumber -> 0
+console.log(Number([1, 2, 3])); // toString -> '1,2,3', toNumber -> NaN
+console.log(Number([[[[[]]]]])); // toString -> '', toNumber -> 0
+
+console.log(Number({})); // toString -> [object Object], toNumber -> NaN
