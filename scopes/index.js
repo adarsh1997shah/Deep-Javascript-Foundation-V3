9d56a0eaee396bf3c89662dc144a678eb9abd356
adarsh1997shah <adarshshah01@gmail.com>
2022-11-21 23:51:50 +0530

    feat: scope notes added

---
+++ scopes/index.js
@@ -0,0 +1,37 @@
+/**
+ * Compiled languages compiles(checks for any errors or create a scope plan) the code before running it
+ * Interpreted languages compiles and runs the code line by line
+
+ * JS is a compiled language as when there is any syntax error, it usually throws error without executing anything above/below it
+ * Or we can say JS gets compiled in two passes
+    - Parsing/Compilation (to generate the plan of scope)
+    - Running/Executing the program
+ * Two king of scopes in JS
+    - Function
+    - Block
+
+ * Lexical scoping is process of looking up to parent scope if the identifier is not defined in the current scope
+
+ * Undefined
+    - A variable is said to be undefined when it has been declared but havent been assigned any value to it in its current scope.
+ * Undeclared
+    - A variable is said to be undeclared when it was never formally declared in any scope that we have access to.
+ */
+
+// Example for a compiled vs interpreted language
+function compiled() {
+	console.log('Run');
+	// console.log()); // Here have added extra braces to generate error, so JS thows error without consoling the above code
+}
+
+function interpreted() {
+	console.log('Run');
+	test(); // Here have added unknown function, which runs code above it i.e the console, before throwing the error test is not defined.
+}
+
+// Example of lexical scope
+let text = 'text';
+
+function lexicalScope() {
+	console.log(text); // Here text is not defined in the function, so it looks for its parents scope.
+}
