d21a3e4cef4f234676628ece3a20c43d74a69d7c
adarsh1997shah <adarshshah01@gmail.com>
2022-11-26 01:20:42 +0530

    feat: block scope notes added

---
+++ advaceScope/blockScope.js
@@ -0,0 +1,25 @@
+/**
+ * Block scope as the name suggest creates a block for scoping.
+ * So instead of an IIFE we can use block scope to seperate/privatise variables in that scope.
+ * Blocks are not scopes unless there is a `let` type variable is declared inside it.
+ */
+
+var number = 12;
+
+{
+	let number = 4;
+	console.log(number); // 4
+}
+
+console.log(numner); // 12
+
+// List of examples of var over let
+function getRandomNumber() {
+	try {
+		var i = getnumber();
+	} catch (error) {
+		var i = Math.random();
+	}
+
+	return i; // As var attaches to function scope and not block scope, it escapes the block scope, semantically appeals the reader that the variable is to be used inside the whole function.
+}
