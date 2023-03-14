033718ed643dd00addc43ee1e32094b0c0290427
adarsh1997shah <adarshshah01@gmail.com>
2023-03-14 15:24:02 +0530

    fix: coersion added more info

--- 2.coercion/index.js
+++ 2.coercion/index.js
@@ -12,13 +12,18 @@
         - first perform toString(), if it gives primitive,
         - then perform valueOf()
 
-      If by performing both the operation we dont get primitive then it generally gives an error
+      If by performing both the operation we don't get primitive then it generally throws an error.
 
   * We have talked about a valueOf and toString a lot, so what those really are
       - valueOf - it returns the primitive value, if it is called upon a non-primitive value then it usually returns the reference of that non-primitive type
-      - Link - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/valueOf
-      
-      - toString() - it turns/converts value to string
+      - Link - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/valueOf.
 
-  * Every primitive and non-primitive value have these two implemented by JS, until and unless they are overridden
+        - By returning primitive value means, usually when we create a variable and assign it a primitive value, like let num = 3, JS does, let num = new Number(3), and it returns the valueOf func value, means it returns the primitive value of this `let num = new Number(3)` declaration.
+
+        - Another question what is the purpose of valueOf ? To this, valueOf is used for comparison.
+        Nice reference here - https://stackoverflow.com/questions/2485632/valueof-vs-tostring-in-javascript
+      
+      - toString() - it turns/converts value returned from the valueOf func to string.
+
+  * Every primitive and non-primitive value have these two implemented by JS, until and unless they are overridden.
  */
